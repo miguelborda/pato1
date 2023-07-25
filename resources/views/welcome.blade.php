99dd54f41b6f36676e7a242d972f314a1f84854b
miguelborda <miguelangelbg20@gmail.com>
2023-07-24 22:48:18 -0400

    4to con mensaje miguel desde casa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Miguel desde casa
                 </div>
 
                 <div class="links">
